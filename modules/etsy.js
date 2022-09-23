5fc8efd25f512838f00cea9a47167dcff109c426
dnuwa <daniel.nuwa@gmail.com>
2022-09-23 14:25:10 +0300

    make sheet name dynamic so user can submit any sheet name

--- modules/etsy.js
+++ modules/etsy.js
@@ -9,16 +9,23 @@ const etsy = async (url) => {
   await page.goto(`${url}`);
 
   const name = await page.evaluate(() =>
-    document.querySelector(".wt-mb-xs-2 h1").innerText.trim()
+    document.querySelector(".wt-mb-xs-2 h1") !== null
+      ? document.querySelector(".wt-mb-xs-2 h1").innerText.trim()
+      : ""
   );
 
   const price = await page.evaluate(() =>
-    document.querySelector(".wt-mb-xs-3 .wt-display-flex-xs p").innerText.trim()
+    document.querySelector(".wt-mb-xs-3 .wt-display-flex-xs p") !== null
+      ? document
+          .querySelector(".wt-mb-xs-3 .wt-display-flex-xs p")
+          .innerText.trim()
+      : ""
   );
 
   let product = {
     name,
     price,
+    error: price === "" ? `null` : ``,
   };
 
   await browser.close();
